0d8cd51239b3a37d5453ce4e2ee99e575458d762
jimMehta <jimit1@gmail.com>
2020-08-22 17:58:49 -0700

    updating template

--- src/App.js
+++ src/App.js
@@ -1,33 +1,82 @@
-import React from "react";
+import React from 'react';
+import './style/App.scss';
 
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+// Components
+import DesktopNav from './components/navbar/desktop-nav';
+import MobileNav from './components/navbar/mobile-nav';
+import Backdrop from "./components/navbar/backdrop";
+import Hero from './components/hero/hero';
+import Portfolio from "./components/portfolio/portfolio";
+import Partners from "./components/partners/partners";
+import About from "./components/about/about";
+import Blog from "./components/blog/blog";
+import Contact from "./components/contact/contact";
+import Footer from "./components/footer/footer";
+ 
+class App extends React.Component {
+  state = {
+    userIsScrolled: false,
+    mobileNavbarOpen: false,
+  };
 
-import "./App.css";
-import Form from "./components/Form/Form";
-import Nav from "./components/Nav/Nav";
-import Text from "./components/Text/Text";
+  componentDidMount() {
+    window.addEventListener("scroll", this.userIsScrolled);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.userIsScrolled);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        {/* <Nav /> */}
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+  // Detect if user is scorlled down (used for add/disable extra large navbar)
+  userIsScrolled = () => {
+    if (window.pageYOffset > 80) {
+      this.setState({ userIsScrolled: true });
+    } else {
+      this.setState({ userIsScrolled: false });
+    }
+  };
+  // On closeMobileMenu click close navbar
+  closeMobileMenu = () => {
+    this.setState({ mobileNavbarOpen: false });
+  };
+  // Mobile menu handler
+  mobileMenuOpen = () => {
+    this.setState({ mobileNavbarOpen: true });
+  };
 
-        <Switch>
-          <Route exact path="/">
-            <Text message="from home page" />
-          </Route>
-          <Route exact path="/about">
-            <Text message="from about page" />
-          </Route>
-          <Route exact path="/contact" component={Form} />
-        </Switch>
-      </Router>
-    </div>
-  );
+  render() {
+    // BACKDROP RENDER
+    let backdrop = <Backdrop closeMobileMenu={this.closeMobileMenu} />;
+    if (this.state.mobileNavbarOpen) {
+      backdrop = (
+        <Backdrop closeMobileMenu={this.closeMobileMenu} isOpen={true} />
+      );
+    }
+    // MOBILE NAVBAR RENDER
+    let mobileNavbar = <MobileNav />;
+    if (this.state.mobileNavbarOpen) {
+      mobileNavbar = (
+        <MobileNav isOpen={true} closeMobileMenu={this.closeMobileMenu} />
+      );
+    }
+
+    return (
+      <div className="App">
+        {mobileNavbar}
+        {backdrop}
+        <DesktopNav
+          userIsScrolled={this.state.userIsScrolled}
+          mobileMenuOpen={this.mobileMenuOpen}
+        />
+        <Hero />
+        <Portfolio />
+        <Partners />
+        <About />
+        <Blog />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
